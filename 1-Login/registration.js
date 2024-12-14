e655cd29cf0d7043baa77dee293543b5e5776ff7
Vishwas-gour <vishwasgour2002@gmail.com>
2024-12-14 20:37:34 +0530

    json created

--- 1-Login/registration.js
+++ 1-Login/registration.js
@@ -2,24 +2,21 @@
 let submit = document.querySelector("#submit-btn");
 let generate = document.querySelector("#generate-btn");
 let otpInput = document.querySelector("#otp");
-console.log(submit, generate, otpInput)
 generate.addEventListener("click", async (e) => {
-    e.preventDefault();
-
+    // e.preventDefault();
     // ---> take the data of all input
     let email = document.getElementById("email").value;
-    let firstName = document.getElementById("first-name").value;
-    let lastName = document.getElementById("last-name").value;
+    let fullName = document.getElementById("full-name").value;
     let password = document.getElementById("password").value;
     let cnfpsw = document.getElementById("confirm-password").value;
+    let id;
     // ---> if inputs are empty
-    if (firstName == "" || email == "" || password == "" || cnfpsw == "") {
+    if (fullName == "" || email == "" || password == "" || cnfpsw == "") {
         alert("all fieild are mandotry");
     }
     else if (password !== cnfpsw) {
         // ---> if password && confirm-password not don't match
         alert("Password not match");
-        return;
     }
     else {
         //  <<<<<<<<<<-IF EVERY-THING-IS-FINE ->>>>>>>>>>>
@@ -33,35 +30,37 @@ generate.addEventListener("click", async (e) => {
         generate.style.display = "none";
         otpInput.style.display = "inline";
         submit.style.width = "40%";
-        submit.style.display = "block";
+        submit.style.display = "inline";
 
         console.log(`Do not share OTP to anyone: %c${otp}`, 'color: orange');
-        alert("check your Gmail")
-        // ---> while registration store all data in localStorage
-        localStorage.setItem("firstName", firstName);
-        localStorage.setItem("lastName", lastName);
-        localStorage.setItem("email", email);
-        localStorage.setItem("password", password);
-
-        submit.addEventListener("click", (e) => {
-            e.preventDefault();
-            // ---> in the case of OTP not match 
-            if (otpInput.value != localStorage.otp) {
-                alert("Wrong OTP try again");
-            } else {
-                // ---> in the case of OTP match render to loginpage 
-
-                alert("New Account has created");
-                window.location.href = "login.html"
-            }
-
-        })
+        alert("check your Gmail");
+        // --->  Set Doctor data in json
+        let url = "http://localhost:3000/Doctor";
+        let obj = fetch(url);
+        try {
+            let resposnse = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify({ id, email, fullName, password }),
+                headers: {
+                    "Contect-Type": "application/json; charset=UTF-8"
+                }
+            });
+        }
+        catch (error) {
+            alert(error);
+        }
+        console.log(response);
     }
 });
-
-
-
-
-
-// TAKE THEME FROM LOCAL STORAGE AND ADD CLASS NAME
-document.body.className = localStorage.theme;
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    // ---> in the case of OTP not match 
+    if (otpInput.value != localStorage.otp) {
+        alert("Wrong OTP try again");
+    }
+    else {
+       // ---> in the case of OTP match render to loginpage 
+       alert("New Account has created");
+       window.location.href = "login.html"
+    }
+});
